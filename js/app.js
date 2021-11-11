32685f20bf5788db50512a2d86e13891c924f1a8
Michael Sunday O <olawuni.michael@gmail.com>
2021-11-11 11:28:06 +0100

    added check for michael

--- js/app.js
+++ js/app.js
@@ -2,5 +2,12 @@ function greetMe() {
 
   const myName = prompt("What is your name? ");
 
-  alert("Welcome " + myName + " . We're glad to have you here. \n Enjoy your stay here. \n Bye Bye. Lols");
+  if (myName === "Michael") {
+    alert("Oh! Michael. You're welcome. \n We are happy to see you")
+  }
+  else {
+
+    alert("Welcome " + myName + " . We're glad to have you here. \n Enjoy your stay here. \n Bye Bye. Lols");
+  }
+
 }
