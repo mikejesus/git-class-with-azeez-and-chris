1c1c243d78b8093852b65e15e801f37ff6f0ba97
Michael Sunday O <olawuni.michael@gmail.com>
2021-11-11 11:21:21 +0100

    Begin work on azeez app

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function greetMe() {
+
+  const myName = prompt("What is your name? ");
+
+  alert("Welcome " + myName + " . We're glad to have you here. \n Enjoy your stay here. \n Bye Bye. Lols");
+}
